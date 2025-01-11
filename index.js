8f6cffe4323ee0d97997c338b89f0e8669d13982
Adeoye Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2025-01-11 15:51:39 +0100

    Initial comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// packages
 import express from "express";
 import bodyParser from "body-parser";
+// retrieves data from the config file .gitignored
 import pool from "./config/db.js";
 
+// config (I do this cause I am more familiar with these names)
 const app = express();
 const port = 3000;
 const db = pool;
 
+// connect do the database
 db.connect();
 
 // Middleware
@@ -22,21 +26,25 @@ app.set("view engine", "ejs");
  *
  */
 
+// to query the data from the database
 async function getBooks() {
   const result = await db.query("SELECT * FROM book_data ORDER BY rating DESC");
   console.log(result.rows);
   return result.rows[0];
 }
 
+// route to handle books page
 app.get("/", async (req, res) => {
   const result = await getBooks();
   res.render("index", {result: result});
 });
 
+// route to render new book page
 app.get("/new", (req, res) => {
   res.render("new-book");
 });
 
+// route to handle adding of books to database
 app.post("/addbook", (req, res) => {
   console.log(req.body);
   const title = req.body.title.toUpperCase();
@@ -51,7 +59,7 @@ app.post("/addbook", (req, res) => {
     "INSERT INTO book_data (title, author, date_read, rating, review, book_cover) VALUES($1, $2, $3, $4, $5, $6)",
     [title, author, dateRead, rating, review, bookCover]
   );
-  res.redirect("/new");
+  res.redirect("/");
 });
 
 app.listen(port, () => {
